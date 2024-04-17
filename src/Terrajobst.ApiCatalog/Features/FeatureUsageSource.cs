046339c52e0c7070ce0e62153d9cde27e8e05853
Immo Landwerth <immol@microsoft.com>
2024-04-16 21:56:31 -0700

    WIP: Proof of concept to show indexing status
    Notes:
    - Not all data sources are created equal:
         - NetFxCompat is basically fixed
         - Upgrade Planner get more data in newer versions, but can't re-index
         - nuget.org can have various version and can re-index
    - Embedding the information in FeatureUsageData is attractive but won't be
      live (unless we change the GenUsage workflows to also update overall usage
      and then ping the website)

--- src/Terrajobst.ApiCatalog/Features/FeatureUsageSource.cs
+++ src/Terrajobst.ApiCatalog/Features/FeatureUsageSource.cs
@@ -13,4 +13,46 @@ public sealed class FeatureUsageSource
     public string Name { get; }
 
     public DateOnly Date { get; }
+
+    public int ReferenceUnitCount
+    {
+        get
+        {
+            return Name switch {
+                "NetFx Compat Lab" => 403,
+                "nuget.org" => 682123,
+                "Upgrade Planner" => 25992
+            };
+        }
+    }
+
+    public IReadOnlyList<(int Version, int ReferenceUnitCount)> VersionDistribution
+    {
+        get
+        {
+            return Name switch {
+                "NetFx Compat Lab" => DistributionNetFxCompatLab,
+                "nuget.org" => DistributionNuGet,
+                "Upgrade Planner" => DistributionUpgradePlanner
+            };
+        }
+    }
+
+    public int SupportedCollectionVersion
+    {
+        get
+        {
+            return Name switch {
+                "NetFx Compat Lab" => 0,
+                "nuget.org" => 2,
+                "Upgrade Planner" => 0
+            };
+        }
+    }
+    
+    private static (int, int)[] DistributionNetFxCompatLab = [(0, 403)];
+
+    private static (int, int)[] DistributionUpgradePlanner = [(0, 25992)];
+
+    private static (int, int)[] DistributionNuGet = [(0, 311287), (2, 370569)];
 }
